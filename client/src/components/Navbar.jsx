3671a43f17d0f150c3c0829fcdcd95929f00461d
slendyG <josuegarza0608@gmail.com>
2023-02-26 00:18:15 -0800

    changes to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,41 +1,11 @@
 import React from "react";
+import { AiOutlineMenu } from 'react-icons/ai';
 
 const Navbar = () => {
-  const links = [
-    {
-      id: 1,
-      link: "Home",
-      href: "/#/",
-    },
-    {
-      id: 2,
-      link: "BookNow",
-      href: "/#/booknow",
-    },
-    {
-      id: 3,
-      link: "Work",
-      href: "/#/work",
-    },
-  ];
   return (
-    <div className="flex justify-between items-center w-full h-20 text-white bg-black px-4">
-      <div>
-        <h1 className="text-4xl text-pink-400 font-signature ml-2">EM Esthetics</h1>
-      </div>
-      <div>
-        <ul className="flex">
-          {links.map(({ id, link, href }) => (
-            <li
-              key={id}
-              className="px-4 cursor-pointer font-medium text-white hover:scale-105 duration-200"
-            >
-              <a href={href}>{link}</a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+   <div>
+    <AiOutlineMenu />
+   </div>
   );
 };
 
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
-import About from '../components/About';
 
 const HomePage = () => {
   return (
-    <div className='h-screen w-full bg-neutral-300'>
-      <div className='max-w-screen-lg mx-auto flex flex-col items-center h-full px-4 md:flex-row'>
-        <div>
-          <img src="" alt="" />
-        </div>
-
-      </div>
-    </div>
+   <div>
+    Homepage
+   </div>
   )
 }
 
